51a6b6e38a4cf21c66b59b7dc809d8e5f2e80a2b
“MaxMap” <975106543@qq.com>
2023-05-28 17:47:08 +0800

    feat: 添加菜单，取消自定义空间

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2023-05-28 09:14:25
+ * @LastEditors: “MaxMap”
+ * @LastEditTime: 2023-05-28 14:23:51
+ * @FilePath: \element-plus-vite-starter\src\plugins\index.ts
+ */
 import {registerRouter} from './router'
 import {registerPinia} from './pinia'
 import { App } from 'vue'
