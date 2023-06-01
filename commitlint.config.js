276a151073d8af2c537263d74aa0749284fcac3a
zhaozhenghao.0 <975106543@qq.com>
2023-06-01 10:10:28 +0800

    feat(custom): git 规范

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,23 +2,22 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [
-            2,
-            'always',
-            [
-                'build', // 主要目的是修改项目构建系统（例如glup，webpack，rollup的配置等）的提交
-                'ci', // 修改项目的持续集成流程（Kenkins、Travis等）的提交
-                'chore', // 构建过程或辅助工具的变化
-                'docs', // 文档提交（documents）
-                'feat', // 新增功能（feature）
-                'fix', // 修复 bug
-                'pref', // 性能、体验相关的提交
-                'refactor', // 代码重构
-                'revert', // 回滚某个更早的提交
-                'style', // 不影响程序逻辑的代码修改、主要是样式方面的优化、修改
-                'test', // 测试相关的开发,
-            ],
-        ],
-    },
+        // type 类型定义
+        'type-enum': [2, 'always', [
+            "feat", // 新功能 feature
+            "fix", // 修复 bug
+            "docs", // 文档注释
+            "style", // 代码格式(不影响代码运行的变动)
+            "refactor", // 重构(既不增加新功能，也不是修复bug)
+            "perf", // 性能优化
+            "test", // 增加测试
+            "chore", // 构建过程或辅助工具的变动
+            "revert", // 回退
+            "build" // 打包
+        ]],
+        // subject 大小写不做校验
+        // 自动部署的BUILD ROBOT的commit信息大写，以作区别
+        'subject-case': [0]
+    }
 };
 
